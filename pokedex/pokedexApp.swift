6a1251dc2e54101d15de42d1bb4f2dfd2742df02
Fahim Khan <fahim.khan@tagitglobal.com>
2024-01-14 04:32:57 +0400

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ragesh on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
